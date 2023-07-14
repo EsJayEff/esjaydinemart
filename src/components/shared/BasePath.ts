8b2cd56c33ccb6ba093fb73cfcdbbb28bad96f23
Shiraz Javed Farooq <114878673+EsJayEff@users.noreply.github.com>
2023-07-14 23:14:22 +0500

    Updated

--- src/components/shared/BasePath.ts
+++ src/components/shared/BasePath.ts
@@ -1,2 +1,2 @@
-const BASE_PATH_FOR_API=process.env.NODE_ENV === "development" ? "http://localhost:3000" : "Vercel Deployed Link"
+const BASE_PATH_FOR_API=process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://esjaydinemart.vercel.app"
 export default BASE_PATH_FOR_API
